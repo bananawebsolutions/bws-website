ec3aaac6e6addf4f919e6eff3ccb0cb0c4b7fee0
Fernando Rojas <fernandorojas@FMA.local>
2024-06-20 17:18:55 -0600

    first commit

---
+++ components/homes/home-1/Hero.jsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero-wrapper hero-1" id="hero">
+      <div className="container">
+        <div className="hero-style1">
+          <div className="row">
+            <div className="col-lg-12">
+              {/* <h1 className="hero-title wow img-custom-anim-left animated">
+                Next Generation
+              </h1> */}
+              <h1 className="hero-title wow animated">Banana</h1>
+
+              {/* <h1 className="hero-title text-lg-end wow img-custom-anim-right animated">
+                Digital Agency
+              </h1> */}
+              <h1 className="hero-title text-lg-end wow animated">
+                Web Solutions
+              </h1>
+            </div>
+            <div className="col-lg-6 offset-lg-6">
+              <p className="hero-text wow animated">
+                Nos dedicamos a ofrecer soluciones web personalizadas que se
+                alinean perfectamente con tus objetivos y necesidades para hacer
+                crecer tu negocio
+              </p>
+              <div className="btn-group fade_right">
+                <Link
+                  scroll={false}
+                  href="/project"
+                  className="btn wow animated"
+                >
+                  <span className="link-effect">
+                    <span className="effect-1">NUESTROS SERVICIOS</span>
+                    <span className="effect-1">NUESTROS SERVICIOS</span>
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="hero-year-tag wow animated">
+            <Image
+              width={40}
+              height={40}
+              src="/assets/img/icon/worldwide.svg"
+              alt="img"
+            />
+            <h6>Agency of this year worldwide</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
