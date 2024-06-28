d60ae5737a9a0e410f71f9d3c5ae6317f71c10cb
Fernando Rojas <fernandorojas@FMA.local>
2024-06-27 19:55:53 -0600

    changed details

--- app/(homes)/home-1/page.jsx
+++ app/(homes)/home-1/page.jsx
@@ -1,16 +1,16 @@
+import MarqueeComponent from "@/components/common/Marquee";
 import Footer1 from "@/components/footers/Footer1";
 import Header1 from "@/components/headers/Header1";
 import SearchPopup from "@/components/headers/component/SearchPopup";
 import Blogs from "@/components/homes/common/Blogs";
-import About from "@/components/homes/home-1/About";
-import Faq from "@/components/homes/home-1/Faq";
-
-import MarqueeComponent from "@/components/common/Marquee";
 import Contact from "@/components/homes/common/Contact";
 import Testimonials from "@/components/homes/common/Testimonials";
+import About from "@/components/homes/home-1/About";
+import Faq from "@/components/homes/home-1/Faq";
 import Hero from "@/components/homes/home-1/Hero";
 import Portfolio from "@/components/homes/home-1/Portfolio";
 import Features2 from "@/components/service/Features2";
+
 export const metadata = {
   title: "Banana Web Solutions",
 };
@@ -22,7 +22,6 @@ export default function HomePage1() {
       <Hero />
       <About />
       <MarqueeComponent />
-
       <Faq />
       <Features2 />
       <Portfolio />
