a9a221d239cdc198a4176ffb032d9ce9a3c0b264
Fernando Rojas <fer.rojas95@gmail.com>
2025-02-03 19:10:38 -0600

    👨🏻‍🔧 fixed not loading pixi at first render

--- components/homes/home-1/HeroAnimation.jsx
+++ components/homes/home-1/HeroAnimation.jsx
@@ -1,7 +1,10 @@
 import Script from "next/script";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 const HeroAnimation = () => {
+    const [step1Loaded, setStep1Loaded] = useState(false);
+    const [step2Loaded, setStep2Loaded] = useState(false);
+    const [step3Loaded, setStep3Loaded] = useState(false);
     const [mounted, setMounted] = useState(false);
     useEffect(() => {
         setMounted(true);
@@ -17,25 +20,30 @@ const HeroAnimation = () => {
 
     return (
         <>
-            {mounted && (
-                <>
-                    <Script
-                        src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"
-                        strategy="afterInteractive"
-                    />
-                    <Script
-                        src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.2/pixi.min.js"
-                        strategy="afterInteractive"
-                    />
-                    <Script
-                        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/PixiPlugin.min.js"
-                        strategy="afterInteractive"
-                    />
-                    <Script
-                        src="/assets/js/hero-animation-bananas.js"
-                        strategy="afterInteractive"
-                    />
-                </>
+            <Script
+                src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"
+                strategy="afterInteractive"
+                onLoad={() => setStep1Loaded(true)}
+            />
+            {step1Loaded && (
+                <Script
+                    src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.2/pixi.min.js"
+                    strategy="afterInteractive"
+                    onLoad={() => setStep2Loaded(true)}
+                />
+            )}
+            {step2Loaded && (
+                <Script
+                    src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/PixiPlugin.min.js"
+                    strategy="afterInteractive"
+                    onLoad={() => setStep3Loaded(true)}
+                />
+            )}
+            {step3Loaded && (
+                <Script
+                    src="/assets/js/hero-animation-bananas.js"
+                    strategy="afterInteractive"
+                />
             )}
         </>
     );
